409ca5fa26e09178143d9d604d3ff18a831ec7c4
MrGrimsz <159098065+MrGrimsz@users.noreply.github.com>
2024-06-14 11:37:27 -0500

    Add files via upload

---
+++ Age Display.py
@@ -0,0 +1,10 @@
+name =  input("what is your name")
+age = input("What is your age")
+exit = input ("Press a key")
+print("")
+
+print("Hello" + " " + name)
+print( age + " " + "Is your age")
+print("")
+
+input()
